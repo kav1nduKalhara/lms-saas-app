aad7bf2aaf4a8a3b18958b79c404fdb8eee00e62
Kavindu840 <kavindu.20220840@iit.ac.lk>
2025-08-17 22:34:09 +0530

    feat:"add  shadcn Button"

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Button} from "@/components/ui/button";
 
 const Page = () => {
   return (
-    <div>Hello</div>
+    <div>
+      <h1 className="text-2xl underline">Welcome to My Saas app</h1>
+      <Button>
+          Let's Get Started
+      </Button>
+    </div>
   )
 }
 
