f7a72a5c8b8a9e996550eba05458bb976017a9af
Kavindu840 <kavindu.20220840@iit.ac.lk>
2025-08-18 19:44:16 +0530

    feat: implement message transcription

--- lib/utils.ts
+++ lib/utils.ts
@@ -54,7 +54,9 @@ export const configureAssistant = (voice: string, style: string) => {
         },
       ],
     },
+    // @ts-expect-error
     clientMessages: [],
+    // @ts-expect-error
     serverMessages: [],
   };
   return vapiAssistant;
